c20e11bac89a7ae5060fd7ba8223651488ccbedd
medlod <50937911+medlod@users.noreply.github.com>
2019-05-23 08:21:18 +0000

    Update main.js

--- main.js
+++ main.js
@@ -36,28 +36,39 @@ bot.on('ready', () => {
     }, 15000)
 });
 
-bot.on('guildMemberAdd', member => {
-  let count = member.guild.memberCount.toString() 
-  let end = count[count.length-1]
-  let suffixed = end == 1 ? count + "st" : end == 2 ? count + "nd" : end == 3 ? count + "rd" : count + "th" 
-  const channel = member.guild.channels.find(chnl => chnl.name === 'welcome');
-  const memberavatar = member.user.displayAvatarURL
-      if (!channel) {
-        console.log("Channel Not found.");
-        return;
-      };
-      
-      let str = `Welcome to the ${member.guild} Community Discord Server, **${member.user.username}**! \nYou are the **${suffixed}** member!
-\nMake sure to check out L-Series' channel and subscribing by clicking [here](https://www.youtube.com/channel/UCodPWCQ7VAXJJiwDV24satw) if you're not already ${member.client.emojis.get("515702715624718346")}` //wrong id?
-      const embed = new Discord.RichEmbed()
-      .setAuthor(member.user.tag, memberavatar)
-      .setColor('363A3F')
-      .setDescription(str)
-      .setThumbnail(memberavatar) // using member.user.displayAvatarURL
-      .setTimestamp();
-      channel.send(embed);
-  
-  console.log(`${member}`, "has joined" + `${member.guild.name}`)
-});
+client.on('guildMemberAdd', async member => {
+	const channel = member.guild.channels.find(ch => ch.name === 'member-log');
+	if (!channel) return;
 
+	const canvas = Canvas.createCanvas(700, 250);
+	const ctx = canvas.getContext('2d');
+
+	const background = await Canvas.loadImage('./wallpaper.jpg');
+	ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
+
+	ctx.strokeStyle = '#74037b';
+	ctx.strokeRect(0, 0, canvas.width, canvas.height);
+
+	// Slightly smaller text placed above the member's display name
+	ctx.font = '28px sans-serif';
+	ctx.fillStyle = '#ffffff';
+	ctx.fillText('Welcome to the server,', canvas.width / 2.5, canvas.height / 3.5);
+
+	// Add an exclamation point here and below
+	ctx.font = applyText(canvas, `${member.displayName}!`);
+	ctx.fillStyle = '#ffffff';
+	ctx.fillText(`${member.displayName}!`, canvas.width / 2.5, canvas.height / 1.8);
+
+	ctx.beginPath();
+	ctx.arc(125, 125, 100, 0, Math.PI * 2, true);
+	ctx.closePath();
+	ctx.clip();
+
+	const avatar = await Canvas.loadImage(member.user.displayAvatarURL);
+	ctx.drawImage(avatar, 25, 25, 200, 200);
+
+	const attachment = new Discord.Attachment(canvas.toBuffer(), 'welcome-image.png');
+
+	channel.send(`Welcome to the server, ${member}!`, attachment);
+});
 bot.login(config.token)
